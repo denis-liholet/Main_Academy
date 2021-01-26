31fbbb8071f892c428e0ae908e8c4d376b8dcf79
pMQlSsNZ <palloncino@ukr.net>
2021-01-26 16:27:09 +0200

    task4 done

--- HM_26_01_21/Task_4.py
+++ HM_26_01_21/Task_4.py
@@ -1,7 +1,7 @@
 # Создать список, содержащий общие для двух данных списков числа
 
-list_1 = [1, 2, 3, 4]
-list_2 = [2, 4, 6, 8]
+list_1 = [1, 2, 3, 4, 2]
+list_2 = [2, 4, 6, 8, 2]
 
-new_lst = [item for item in list_1 if item in list_2]
+new_lst = [item for item in set(list_1) if item in set(list_2)]
 print(new_lst)
