6efa0b9fa528606e2418f2a175a679a6dd1564f0
pMQlSsNZ <palloncino@ukr.net>
2021-01-26 16:30:46 +0200

    task3 done

--- HM_26_01_21/Task_3.py
+++ HM_26_01_21/Task_3.py
@@ -1,4 +1,4 @@
 # Найти все цифры от 1 до 1000, которые содержат в себе 3
 
-new_lst = [item for item in [i for i in range(1, 1001)] if '3' in str(item)]
+new_lst = [item for item in range(1, 1001) if '3' in str(item)]
 print(new_lst)
